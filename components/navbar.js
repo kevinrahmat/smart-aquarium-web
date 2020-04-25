415adb4233c98180cd0c9dbb5a7981d726bace9b
tmfadhlul <tmfadhlul@gmail.com>
2020-04-25 21:46:32 +0700

    change navbar link router

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default class Navbar extends React.Component {
   render () {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-bright-gray ">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Smartify</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/">Dashboard <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="/controller">Controller</a>
-            <a class="nav-item nav-link" href="/login">Login</a>
-            <a class="nav-item nav-link" href="/register">Register</a>
+            <Link href="/">
+              <a className="nav-link nav-link">Dashboard</a>
+            </Link>
+            <Link href="/controller">
+              <a className="nav-link nav-link">Controller</a>
+            </Link>
+            <Link href="/login">
+              <a className="nav-link nav-link">Login</a>
+            </Link>
+            <Link href="/register">
+              <a className="nav-link nav-link">Register</a>
+            </Link>
           </div>
         </div>
       </nav>
